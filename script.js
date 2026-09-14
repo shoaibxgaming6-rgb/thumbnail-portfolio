f315f2c42eea5a2ff8c93d539e51347f86cd92c8
shoaibxgaming6-rgb <shoaibxgaming6@gmail.com>
2026-09-14 15:30:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const button=document.querySelector('.menu-toggle'),nav=document.querySelector('nav');button?.addEventListener('click',()=>{nav.classList.toggle('open');button.textContent=nav.classList.contains('open')?'Close':'Menu'});
